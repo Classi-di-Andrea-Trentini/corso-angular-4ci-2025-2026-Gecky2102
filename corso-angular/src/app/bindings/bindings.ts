6c91aaca3139aefd8071b258b8e8890a57e5bde8
Giacomo Masiero <90463906+Gecky2102@users.noreply.github.com>
2025-10-27 11:05:09 +0000

    feat: aggiungere funzionalità di contatore e immagini nel componente Bindings

--- corso-angular/src/app/bindings/bindings.ts
+++ corso-angular/src/app/bindings/bindings.ts
@@ -10,6 +10,41 @@ export class Bindings {
   // Dichiarazione delle variabili prevede: nome: tipo = valore iniziale
   studente: WritableSignal<string> = signal("Giacomo Masiero");
 
+  contatore: WritableSignal<number> = signal(0);
+
+  immagini: string[] = [
+    '/bicicletta_1.jpg',
+    '/bicicletta_2.jpg',
+    '/bicicletta_3.jpg',
+    '/bicicletta_4.jpg',
+    '/bicicletta_5.jpg',
+    '/bicicletta_6.jpg',
+    '/bicicletta_7.jpg',
+    '/bicicletta_8.jpg',
+    '/bicicletta_9.jpg',
+    '/bicicletta_10.jpg',
+    '/bicicletta_11.jpg',
+    '/bicicletta_12.jpg',
+    '/bicicletta_13.jpg',
+    '/bicicletta_14.jpg',
+    '/bicicletta_15.jpg'
+  ];
+  indiceImmagine: WritableSignal<number> = signal(0);
+
+
+  incrementaContatore(): void {
+
+    // meglio di this.contatore.set(this.contatore() + 1);
+    // più elegante e il metodo update acceta due paramentri:
+    // il valore precedente e il nuovo valore
+    this.contatore.update(valorePrecedente => valorePrecedente + 1);
+
+  }
+
+  decrementaContatore(): void {
+    this.contatore.update(valorePrecedente => valorePrecedente - 1);
+  }
+
   cambiaNome(): void {
     // Corpo del metodo
     this.studente.set("Gabriele Pittui");
