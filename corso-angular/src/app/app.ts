e89751b700068bd47f8c555cdf3c756c263029be
Giacomo Masiero <90463906+Gecky2102@users.noreply.github.com>
2025-10-27 10:10:57 +0000

    feat: aggiungere il componente Footer con template e stili

--- corso-angular/src/app/app.ts
+++ corso-angular/src/app/app.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { Bindings } from './bindings/bindings';
 import { NavBar } from './nav-bar/nav-bar';
 import { Header } from './header/header';
+import { Footer } from './footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Bindings, NavBar, Header],
+  imports: [RouterOutlet, Bindings, NavBar, Header, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
