6936c92f2f89b5f742655a9783e6ef1f87e2f348
Giacomo Masiero <90463906+Gecky2102@users.noreply.github.com>
2025-11-24 10:55:42 +0000

    feat: aggiornare il template principale per includere il componente ComunicazioneFraComponenti

--- corso-angular/src/app/app.ts
+++ corso-angular/src/app/app.ts
@@ -6,10 +6,11 @@ import { Header } from './header/header';
 import { Footer } from './footer/footer';
 import { StructuralDirectives } from './structural-directives/structural-directives';
 import { ClassiInterfacce } from './classi-interfacce/classi-interfacce';
+import { ComunicazioneFraComponenti } from "./comunicazione-fra-componenti/comunicazione-fra-componenti";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Bindings, NavBar, Header, Footer, StructuralDirectives, ClassiInterfacce],
+  imports: [RouterOutlet, Bindings, NavBar, Header, Footer, StructuralDirectives, ClassiInterfacce, ComunicazioneFraComponenti],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
