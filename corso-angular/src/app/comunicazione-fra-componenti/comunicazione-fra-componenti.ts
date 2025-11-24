b0a8f5d3b23833443bab2eb357aae7d2706db0ef
Giacomo Masiero <90463906+Gecky2102@users.noreply.github.com>
2025-11-24 11:04:22 +0000

    feat: aggiornare il componente ComunicazioneFraComponenti per gestire l'aggiunta di docenti e visualizzarli

--- corso-angular/src/app/comunicazione-fra-componenti/comunicazione-fra-componenti.ts
+++ corso-angular/src/app/comunicazione-fra-componenti/comunicazione-fra-componenti.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, signal, WritableSignal } from '@angular/core';
 import { VisualizzaElenco } from './visualizza-elenco/visualizza-elenco';
+import { IDocente } from '../classi-interfacce/i-docente';
 
 @Component({
   selector: 'app-comunicazione-fra-componenti',
@@ -9,7 +10,18 @@ import { VisualizzaElenco } from './visualizza-elenco/visualizza-elenco';
 })
 export class ComunicazioneFraComponenti {
 
-  
+  docenti: WritableSignal<IDocente[]> = signal([]);
 
+  aggiungiDocenti() {
+    const elencoDocenti: IDocente[] = [
+      {id: 1, nome: 'Andrea', cognome: 'Trentini', classi: [], materie: []},
+      {id: 2, nome: 'Federico', cognome: 'Sannicolò', classi: [], materie: []},
+      {id: 3, nome: 'Denis', cognome: 'Tairovski', classi: [], materie: []},
+      {id: 4, nome: 'Mauro', cognome: 'Longano', classi: [], materie: []},
+      {id: 5, nome: 'Federico', cognome: 'Bertolli', classi: [], materie: []},
+      {id: 6, nome: 'Patrizia', cognome: 'Montagni', classi: [], materie: []}
+    ];
+    this.docenti.set(elencoDocenti);
+  }
 
 }
--- corso-angular/src/app/comunicazione-fra-componenti/visualizza-elenco/visualizza-elenco.ts
+++ corso-angular/src/app/comunicazione-fra-componenti/visualizza-elenco/visualizza-elenco.ts
@@ -11,5 +11,5 @@ export class VisualizzaElenco {
 
   dati = input.required<IDocente[]>();
   
-
+  
 }
