454aab3d693d1d7847e150b020adc3b018166c06
Giacomo Masiero <90463906+Gecky2102@users.noreply.github.com>
2025-11-24 10:55:46 +0000

    feat: aggiungere il componente VisualizzaElenco e aggiornare il template ComunicazioneFraComponenti

--- corso-angular/src/app/comunicazione-fra-componenti/comunicazione-fra-componenti.ts
+++ corso-angular/src/app/comunicazione-fra-componenti/comunicazione-fra-componenti.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { VisualizzaElenco } from './visualizza-elenco/visualizza-elenco';
 
 @Component({
   selector: 'app-comunicazione-fra-componenti',
-  imports: [],
+  imports: [VisualizzaElenco],
   templateUrl: './comunicazione-fra-componenti.html',
   styleUrl: './comunicazione-fra-componenti.css'
 })
 export class ComunicazioneFraComponenti {
+
+  
+
 
 }
